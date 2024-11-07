041d6498a3e31f04969d12a6d26c0c829596b59c
“Andrey <aiegorovceo@yandex.ru>
2024-11-07 13:31:59 +0300

    feat: add User entity class

---
+++ src/main/java/hw2/model/User.java
@@ -0,0 +1,21 @@
+package hw2.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    public User(int id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
+
