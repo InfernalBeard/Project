012728772ff426ff37d3067d923e2210a7f8974a
“Andrey <aiegorovceo@yandex.ru>
2024-11-07 13:55:08 +0300

    feat: create UserServlet for handling user-related HTTP requests

---
+++ src/main/java/hw2/servlet/UserServlet.java
@@ -0,0 +1,32 @@
+package hw2.servlet;
+
+
+import hw2.dao.UserDao;
+import hw2.model.User;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class UserServlet extends HttpServlet {
+    private UserDao userDao = new UserDao();
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+
+        User user = new User(0, name, email);
+        try {
+            userDao.saveUser(user);
+            response.getWriter().write("User saved successfully.");
+        } catch (SQLException e) {
+            response.getWriter().write("Error saving user.");
+            e.printStackTrace();
+        }
+    }
+}
+
