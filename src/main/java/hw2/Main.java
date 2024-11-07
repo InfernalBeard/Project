9469e2fe72081b45e02c39857f2fc27992a16e46
“Andrey <aiegorovceo@yandex.ru>
2024-11-07 14:45:29 +0300

    feat: update Main

--- src/main/java/hw2/Main.java
+++ src/main/java/hw2/Main.java
@@ -7,24 +7,18 @@ import hw2.servlet.UserServlet;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Create a Jetty server instance, running on port 8080
         Server server = new Server(8080);
 
-        // Create a ServletContextHandler to map servlets
         ServletContextHandler contextHandler = new ServletContextHandler(ServletContextHandler.SESSIONS);
         contextHandler.setContextPath("/"); // The root context
 
-        // Map UserServlet to handle POST requests at "/user"
         contextHandler.addServlet(new ServletHolder(new UserServlet()), "/user");
 
-        // Set the context for the server
         server.setHandler(contextHandler);
 
-        // Start the server
         System.out.println("Starting server on port 8080...");
         server.start();
 
-        // Join the server thread to wait for it to finish (it will run until interrupted)
         server.join();
     }
 }
